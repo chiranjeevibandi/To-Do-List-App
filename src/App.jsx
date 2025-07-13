b8f9ee53c3606a1ea70ed3360484816466e6196c
bandhichiranjeevi <chiranjeevibandi29@gmail.com>
2025-07-13 08:55:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ToDoList from './ToDoList'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App(){
+  return (
+    <ToDoList></ToDoList>
+  )
+}
+
+export default App
